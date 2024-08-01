3bfbe9bf9feb2080cbbf1a4aba8e8e2fb607733d
Rodrigo do Vale Stankowicz <rodrigo.vale.st@gmail.com>
2024-07-31 21:01:49 -0300

    [test] unit tests to title component

--- frontend/src/app/components/title/title.component.spec.ts
+++ frontend/src/app/components/title/title.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TitleComponent } from './title.component';
 
-describe('TitleComponent', () => {
+fdescribe('TitleComponent', () => {
   let component: TitleComponent;
   let fixture: ComponentFixture<TitleComponent>;
 
@@ -19,5 +19,27 @@ describe('TitleComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should bind "text" in template', async () => {
+    const mockedInputTitle = 'test-title';
+    fixture.componentRef.setInput('title', mockedInputTitle);
+    fixture.detectChanges();
+
+    await fixture.whenStable();
+
+    const button: HTMLElement = fixture.nativeElement.querySelector('h2');
+    expect(button.textContent).toContain(mockedInputTitle);
+  });
+
+  it('should bind the attribute "class" in outer div class on template', async () => {
+    const mockedInputClass = 'test-class';
+    fixture.componentRef.setInput('class', mockedInputClass);
+    fixture.detectChanges();
+
+    await fixture.whenStable();
+
+    const div: HTMLElement = fixture.nativeElement.querySelector('div');
+    expect(div.classList).toContain(mockedInputClass);
   });
 });
