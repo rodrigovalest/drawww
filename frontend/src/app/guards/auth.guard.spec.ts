8565a98924e28029be748559741d812c8aca0714
Rodrigo do Vale Stankowicz <rodrigo.vale.st@gmail.com>
2024-08-02 14:04:50 -0300

    src/app/guards/auth.guard.spec.ts

--- frontend/src/app/guards/auth.guard.spec.ts
+++ frontend/src/app/guards/auth.guard.spec.ts
@@ -1,17 +1,48 @@
 import { TestBed } from '@angular/core/testing';
-import { CanMatchFn } from '@angular/router';
+import { CanMatchFn, Router } from '@angular/router';
 
 import { authGuard } from './auth.guard';
+import { AuthService } from '../services/auth.service';
 
 describe('authGuard', () => {
+  let authService: jasmine.SpyObj<AuthService>;
+  let router: jasmine.SpyObj<Router>;
   const executeGuard: CanMatchFn = (...guardParameters) => 
       TestBed.runInInjectionContext(() => authGuard(...guardParameters));
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const authServiceSpy = jasmine.createSpyObj('AuthService', ['isLoggedIn']);
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: AuthService, useValue: authServiceSpy },
+        { provide: Router, useValue: routerSpy }
+      ]
+    });
+
+    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
   });
 
   it('should be created', () => {
     expect(executeGuard).toBeTruthy();
+  });
+
+  it('should allow access if user is logged in', () => {
+    authService.isLoggedIn.and.returnValue(true);
+
+    const result = executeGuard({ path: 'test' } as any, [] as any);
+
+    expect(result).toBe(true);
+  });
+
+  it('should navigate to login if user is not logged in', () => {
+    authService.isLoggedIn.and.returnValue(false);
+
+    const result = executeGuard({ path: 'test' } as any, [] as any);
+
+    expect(result).toBeUndefined();
+    expect(router.navigate).toHaveBeenCalledWith(['login']);
   });
 });
