57e7e1f0b0af2c1a9fa04f880ab7a0ebd483a6fd
Rodrigo do Vale Stankowicz <rodrigo.vale.st@gmail.com>
2024-08-05 15:11:05 -0300

    [add] page not found interface

--- frontend/src/app/pages/page-not-found/page-not-found.component.ts
+++ frontend/src/app/pages/page-not-found/page-not-found.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { TitleComponent } from "../../components/title/title.component";
+import { LinkComponent } from "../../components/link/link.component";
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [],
+  imports: [TitleComponent, LinkComponent],
   templateUrl: './page-not-found.component.html',
 })
 export class PageNotFoundComponent {
-
 }
