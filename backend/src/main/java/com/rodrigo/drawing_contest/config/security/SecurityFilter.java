14cdf62bff93e7f8247466757b0c10741dd0973b
Rodrigo do Vale Stankowicz <rodrigo.vale.st@gmail.com>
2024-08-05 19:33:41 -0300

    [mod] moving exception response dto and handler

--- backend/src/main/java/com/rodrigo/drawing_contest/config/security/SecurityFilter.java
+++ backend/src/main/java/com/rodrigo/drawing_contest/config/security/SecurityFilter.java
@@ -1,5 +1,6 @@
 package com.rodrigo.drawing_contest.config.security;
 
+import com.rodrigo.drawing_contest.models.user.User;
 import com.rodrigo.drawing_contest.models.user.UserDetails;
 import com.rodrigo.drawing_contest.services.JwtService;
 import com.rodrigo.drawing_contest.services.UserService;
@@ -28,21 +29,21 @@ public class SecurityFilter extends OncePerRequestFilter {
             HttpServletResponse response,
             FilterChain filterChain
     ) throws ServletException, IOException {
-        String token = this.recoverToken(request);
+        String authHeader = request.getHeader("Authorization");
+        String token = (authHeader != null) ? authHeader.replace("Bearer ", "") : null;
 
         if (token != null) {
             String username = this.jwtService.getUsernameByToken(token);
-            UserDetails userDetails = new UserDetails(this.userService.findUserByUsername(username));
+            User user = this.userService.findUserByUsername(username);
 
-            var authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+            if (user != null) {
+                UserDetails userDetails = new UserDetails(user);
+
+                var authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
         }
 
         filterChain.doFilter(request, response);
-    }
-
-    private String recoverToken(HttpServletRequest request) {
-        String authHeader = request.getHeader("Authorization");
-        return (authHeader != null) ? authHeader.replace("Bearer ", "") : null;
     }
 }
--- backend/src/main/java/com/rodrigo/drawing_contest/controllers/RestExceptionHandler.java
+++ backend/src/main/java/com/rodrigo/drawing_contest/controllers/RestExceptionHandler.java
@@ -1,5 +1,7 @@
-package com.rodrigo.drawing_contest.exceptions;
+package com.rodrigo.drawing_contest.controllers;
 
+import com.rodrigo.drawing_contest.dtos.response.RestErrorMessage;
+import com.rodrigo.drawing_contest.exceptions.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
