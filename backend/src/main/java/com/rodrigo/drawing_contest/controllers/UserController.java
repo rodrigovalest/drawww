b70093849fe6445cdcff2ac6b8695b6812979286
Rodrigo do Vale Stankowicz <rodrigo.vale.st@gmail.com>
2024-08-15 17:40:51 -0300

    [mod] user controller changes to dto refactor

--- backend/src/main/java/com/rodrigo/drawing_contest/controllers/UserController.java
+++ backend/src/main/java/com/rodrigo/drawing_contest/controllers/UserController.java
@@ -1,19 +1,14 @@
 package com.rodrigo.drawing_contest.controllers;
 
 import com.rodrigo.drawing_contest.models.user.User;
-import com.rodrigo.drawing_contest.models.user.UserDetailsImpl;
 import com.rodrigo.drawing_contest.services.JwtService;
 import com.rodrigo.drawing_contest.services.UserService;
-import com.rodrigo.drawing_contest.dtos.request.LoginRequestDto;
-import com.rodrigo.drawing_contest.dtos.request.RegisterRequestDto;
-import com.rodrigo.drawing_contest.dtos.response.LoginResponseDto;
+import com.rodrigo.drawing_contest.dtos.http.request.LoginRequestDto;
+import com.rodrigo.drawing_contest.dtos.http.request.RegisterRequestDto;
+import com.rodrigo.drawing_contest.dtos.http.response.LoginResponseDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -39,11 +34,5 @@ public class UserController {
         User user = this.userService.createUser(dto.getUsername(), dto.getPassword());
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
         return ResponseEntity.created(location).build();
-    }
-
-    @GetMapping("/hello")
-    public ResponseEntity<String> hello(Authentication authentication) {
-        System.out.println(authentication.toString());
-        return ResponseEntity.ok("HelloWorld");
     }
 }
