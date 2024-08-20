63529c68ffdfb348d4838ef1f5dfd8dd33a47b6b
Rodrigo do Vale Stankowicz <rodrigo.vale.st@gmail.com>
2024-08-20 20:47:31 -0300

    [add] logout route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,10 +6,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { authGuard } from './guards/auth.guard';
 import { CreatePrivateRoomComponent } from './components/create-private-room/create-private-room.component';
 import { PrivateRoomComponent } from './pages/private-room/private-room.component';
+import { LogoutComponent } from './pages/logout/logout.component';
 
 export const routes: Routes = [
     { path: "", component: HomeComponent, canActivate: [authGuard] },
     { path: "private", component: PrivateRoomComponent },
+    { path: "logout", component: LogoutComponent },
     { path: "test", component: CreatePrivateRoomComponent },
     { path: "login", component: LoginComponent },
     { path: "signup", component: SignupComponent },
